3a055899249bc4eefbb031fe31407fc39e59b948
DucHuyHoang <huyhoang10032000@gmail.com>
2022-10-14 00:38:50 +0700

    fix image size and add image wrapper

--- components/shared/post/PostImage.tsx
+++ components/shared/post/PostImage.tsx
@@ -2,12 +2,17 @@ import React from 'react'
 
 const PostImage = ({ src, ...rest }) => {
   return (
-    // <div
-    //   className="w-100"
-    //   style={{ height: '250px', borderRadius: '5px', overflow: 'hidden' }}
-    // >
-    <img className="w-100" style={{ objectFit: 'cover',borderRadius: '5px', overflow: 'hidden' }} src={src} {...rest} />
-    // </div>
+    <div
+      className="w-100"
+      style={{ height: '250px', borderRadius: '5px', overflow: 'hidden' }}
+    >
+      <img
+        className="w-100"
+        style={{ objectFit: 'cover', borderRadius: '5px', overflow: 'hidden' }}
+        src={src}
+        {...rest}
+      />
+    </div>
   )
 }
 export default PostImage
