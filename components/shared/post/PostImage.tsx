efaf8601cf1df03a6c6d19b4cead0dc93220e9e7
DucHuyHoang <huyhoang10032000@gmail.com>
2022-10-17 00:45:59 +0700

    init blockchain term mdx

--- components/shared/post/PostImage.tsx
+++ components/shared/post/PostImage.tsx
@@ -4,7 +4,7 @@ const PostImage = ({ src, ...rest }) => {
   return (
     <div
       className="w-100"
-      style={{ height: '250px', borderRadius: '5px', overflow: 'hidden' }}
+      style={{ maxHeight: '500px', borderRadius: '5px', overflow: 'hidden' }}
     >
       <img
         className="w-100"
