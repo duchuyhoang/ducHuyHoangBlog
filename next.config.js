987a0fe5873aa2d0861ac454c2bb7fc756bcb6c2
Huy.Hoang2 <huy.hoang2@sotatek.com>
2022-12-12 14:40:27 +0700

    update build info

--- next.config.js
+++ next.config.js
@@ -2,36 +2,42 @@
 module.exports = {
   reactStrictMode: false,
   images: {
-    domains: ["about.gitlab.com"],
+    domains: ['about.gitlab.com'],
+    loader: 'akamai',
+    path: ''
   },
-  eslint:{
-	ignoreDuringBuilds: true,
+  eslint: {
+    ignoreDuringBuilds: true
   }
-};
-const withMDX = require("@next/mdx")({
+}
+const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
   options: {
     remarkPlugins: [],
     rehypePlugins: [],
     // If you use `MDXProvider`, uncomment the following line.
-    providerImportSource: "@mdx-js/react",
+    providerImportSource: '@mdx-js/react'
   },
   images: {
-    domains: ["about.gitlab.com"],
-  },
-});
-const withTM = require("next-transpile-modules")([
-  "gsap",
-  "hover-effect",
-  "react-syntax-highlighter"
-]);
-module.exports = withTM(withMDX({
-  // Append the default value with md extensions
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-  images: {
-    domains: ["about.gitlab.com"],
-  },
-  eslint:{
-	ignoreDuringBuilds: true,
+    domains: ['about.gitlab.com']
   }
-}));
+})
+const withTM = require('next-transpile-modules')([
+  'gsap',
+  'hover-effect',
+  'react-syntax-highlighter'
+])
+module.exports = withTM(
+  withMDX({
+    // Append the default value with md extensions
+    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+    images: {
+      domains: ['about.gitlab.com'],
+      loader: 'akamai',
+      path: ''
+    },
+    eslint: {
+      ignoreDuringBuilds: true
+    }
+  })
+)
